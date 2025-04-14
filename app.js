db8f35145bae63a12f2102e17846bcda12463602
AbinayaKiruthiga <abinayakiruthiga21@gmail.com>
2025-04-14 15:33:19 +0530

    app.js change in feature

---
+++ app.js
@@ -0,0 +1,9 @@
+// Take input from the user
+let num1 = parseFloat(prompt("Enter the first number:"));
+let num2 = parseFloat(prompt("Enter the second number:"));
+
+// Add the numbers
+let sum = num1 + num2;
+
+// Show the result
+alert("Sum: " + sum);
